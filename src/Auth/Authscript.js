cd114cda64002456c7d323054f38927cf3f6d99e
maxime <maximelarquetoux@gmail.com>
2025-12-18 10:29:16 +0100

    Creating the logic for Connection

--- src/Auth/Authscript.js
+++ src/Auth/Authscript.js
@@ -36,3 +36,35 @@ if (signupForm) {
         window.location.href = "../Auth/SigninPage.html";
     });
 }
+
+
+const signInForm = document.getElementById('signIn');
+
+if (signInForm) {
+    // Function submit on SignInForm
+    signInForm.addEventListener('submit', function (event) {
+
+        event.preventDefault();
+
+        // Get all datas we need on Sign-In Page
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+
+        // Get the key with email
+        const storedDatas = localStorage.getItem(email);
+
+        if (!storedDatas) {
+            alert("l'email de correspond pas !")
+        }
+
+        // Transform the datas in JSON for get the password
+        const userObject = JSON.parse(storedDatas);
+
+        // Compare the storedPassword with the actual password
+        if (userObject.password !== password) {
+            alert("le mot de passe de correspond pas !")
+        } else {
+            console.log("Match ! Connexion en cours...");
+        }
+    })
+}
