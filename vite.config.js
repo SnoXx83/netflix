ee183b27a75562ebfd25d99ac5c0bcce40023090
maxime <maximelarquetoux@gmail.com>
2025-12-23 12:11:51 +0100

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,16 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/nestflux/', // Indispensable pour GitHub Pages
+  base: '/nestflux/',
   build: {
     rollupOptions: {
       input: {
-        // On indique à Vite où sont tous les fichiers HTML
         main: resolve(__dirname, 'index.html'),
         signup: resolve(__dirname, 'src/Auth/SignupPage.html'),
         signin: resolve(__dirname, 'src/Auth/SigninPage.html'),
-        config: resolve(__dirname, 'src/user/Config.html'),
-        profil: resolve(__dirname, 'src/user/Profil.html'),
+        session: resolve(__dirname, 'src/user/Session.html'),
+        profil: resolve(__dirname, 'src/user/Profile.html'),
       },
     },
   },
-})
+});
